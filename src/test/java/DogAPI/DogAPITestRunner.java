73401954cbe75b2048f6e41f1dcafe3dc0cca496
Reitumetse Mahloko <reemahloko@gmail.com>
2023-03-17 09:18:59 +0200

    updated

--- src/test/java/DogAPI/DogAPITestRunner.java
+++ src/test/java/DogAPI/DogAPITestRunner.java
@@ -1,38 +1,43 @@
-package DogAPI;
+package features;
 
 import com.intuit.karate.Results;
 import com.intuit.karate.Runner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 import com.intuit.karate.junit5.Karate;
 import net.masterthought.cucumber.Configuration;
 import net.masterthought.cucumber.ReportBuilder;
 import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
-public class DogAPITestRunner {
+class DogAPITestRunner {
+
+    @BeforeAll
+    public static void initialiseObjects() {
+
+    }
 
     @Karate.Test
-    Karate DogAPI() {
-
+    Karate testVotes() {
         Results results = Runner.path("classpath:DogAPI").tags("~@ignore").outputCucumberJson(true).parallel(5);
         generateReport(results.getReportDir());
-        return Karate.run().relativeTo(getClass());
 
+        return null;
     }
-
     public static void generateReport(String karateOutputPath) {
         Collection<File> jsonFiles = FileUtils.listFiles(new File(karateOutputPath), new String[]{"json"}, true);
-        final List<String> jsonPaths = new ArrayList<>(jsonFiles.size());
-
+        final List<String> jsonPaths = new ArrayList(jsonFiles.size());
         jsonFiles.forEach(file -> jsonPaths.add(file.getAbsolutePath()));
-
-        File file = new File("target");
-
-        Configuration con = new Configuration(file, "karateDogAPI");
-        ReportBuilder reportBuilder = new ReportBuilder(jsonPaths, con);
+        Configuration config = new Configuration(new File("target"), "demo");
+        ReportBuilder reportBuilder = new ReportBuilder(jsonPaths, config);
         reportBuilder.generateReports();
     }
+
 }
